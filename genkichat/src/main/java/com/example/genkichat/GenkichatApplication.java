48695f0525b96208979d32ea3833762a6fd8b08d
nakamata0049 <kine2enik@gmail.com>
2025-05-08 13:35:32 +0900

    a

---
+++ genkichat/src/main/java/com/example/genkichat/GenkichatApplication.java
@@ -0,0 +1,13 @@
+package com.example.genkichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenkichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenkichatApplication.class, args);
+	}
+
+}
---
+++ genkichat/src/test/java/com/example/genkichat/GenkichatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genkichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenkichatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
